01098810db6e1387449df2fe14ee45319be769d5
Qwest159 <duchesne-raphael@hotmail.com>
2024-11-26 23:09:51 +0100

    couleur/image + vue

--- src/components/TypePokemon.vue
+++ src/components/TypePokemon.vue
@@ -1,2 +1,22 @@
-<script setup></script>
-<template>test</template>
+<script setup>
+import { defineProps, ref } from "vue";
+const props = defineProps({
+  id: {
+    type: String,
+  },
+});
+let pokemonList = ref([]);
+const pokemon_id = props.id;
+
+let donnees_localstorage = JSON.parse(localStorage.getItem("pokemon"));
+
+pokemonList.value = donnees_localstorage[pokemon_id - 1];
+console.log(pokemon_id);
+
+console.log(donnees_localstorage[pokemon_id - 1]);
+</script>
+<template>
+  <figure class="">
+    <img class="w-6" src="../../public/image/types/plante.png" alt="" />
+  </figure>
+</template>
--- src/views/ShowView.vue
+++ src/views/ShowView.vue
@@ -18,12 +18,79 @@ const pokemon_id = props.id;
 const version_actuelle = version;
 
 let pokemonList = ref([]);
+let pokemonType = ref();
+let couleurBackground = ref();
 const isLoading = ref(true);
 const error = ref(null);
 
 async function fetchPokemonList() {
   let donnees_localstorage = JSON.parse(localStorage.getItem("pokemon"));
   let version_localstorage = JSON.parse(localStorage.getItem("version"));
+  const typeCouleur = {
+    grass: {
+      image: "../../public/image/types/plante.png",
+      couleur: "bg-green-600",
+    },
+    bug: {
+      image: "../../public/image/types/plante.png",
+      couleur: "bg-green-600",
+    },
+    fire: {
+      image: "../../public/image/types/feu.png",
+      couleur: "bg-red-600",
+    },
+    water: {
+      image: "../../public/image/types/eau.png",
+      couleur: "bg-cyan-500",
+    },
+    ice: {
+      image: "../../public/image/types/eau.png",
+      couleur: "bg-cyan-500",
+    },
+    fighting: {
+      image: "../../public/image/types/combat.png",
+      couleur: "bg-yellow-700",
+    },
+    rock: {
+      image: "../../public/image/types/combat.png",
+      couleur: "bg-yellow-700",
+    },
+    ground: {
+      image: "../../public/image/types/combat.png",
+      couleur: "bg-yellow-700",
+    },
+    dragon: {
+      image: "../../public/image/types/dragon.png",
+      couleur: "bg-yellow-600",
+    },
+    electric: {
+      image: "../../public/image/types/electricite.png",
+      couleur: "bg-yellow-400",
+    },
+    metal: {
+      image: "../../public/image/types/metal.png",
+      couleur: "bg-gray-600",
+    },
+    psychic: {
+      image: "../../public/image/types/psy.png",
+      couleur: "bg-purple-500",
+    },
+    dark: {
+      image: "../../public/image/types/tenebre.png",
+      couleur: "bg-gray-500",
+    },
+    normal: {
+      image: "../../public/image/types/normal.png",
+      couleur: "bg-gray-300",
+    },
+    poison: {
+      image: "../../public/image/types/psy.png",
+      couleur: "bg-purple-500",
+    },
+  };
+
+  console.log(donnees_localstorage[pokemon_id - 1].type);
+
   // console.log(donnees_localstorage[pokemon_id]);
 
   if (
@@ -33,10 +100,20 @@ async function fetchPokemonList() {
   ) {
     console.log("1");
     isLoading.value = false;
-
     pokemonList.value = donnees_localstorage[pokemon_id - 1];
 
-    // console.log(pokemonList);
+    let imageType1 =
+      typeCouleur[donnees_localstorage[pokemon_id - 1].type[0]]?.image;
+    let imageType2 =
+      typeCouleur[donnees_localstorage[pokemon_id - 1].type[1]]?.image;
+    pokemonType.value = imageType1 || imageType2;
+
+    let ColorType1 =
+      typeCouleur[donnees_localstorage[pokemon_id - 1].type[0]]?.couleur;
+    let ColorType2 =
+      typeCouleur[donnees_localstorage[pokemon_id - 1].type[1]]?.couleur;
+    couleurBackground = ColorType1 || ColorType2;
+    console.log(couleurBackground);
   } else {
     try {
       console.log("2");
@@ -69,20 +146,37 @@ async function fetchPokemonList() {
         }
         new_weight += transformweight[index];
       }
+      console.log(data.types[0].type.name);
 
       donnees_localstorage[pokemon_id - 1] = {
         id: pokemon_id,
         name: data.name,
         weight: new_weight,
-        type: data.types[0].type.name,
+        type: [data.types[0].type.name, data.types[1]?.type.name].filter(
+          Boolean
+        ),
       };
 
       // DONNEE STORAGE
       localStorage.setItem("version", JSON.stringify(version_actuelle));
 
       localStorage.setItem("pokemon", JSON.stringify(donnees_localstorage));
+
+      //DONNEE A AFFICHER
       pokemonList.value = donnees_localstorage[pokemon_id - 1];
-      console.log(pokemonList.value);
+
+      let imageType1 =
+        typeCouleur[donnees_localstorage[pokemon_id - 1].type[0]]?.image;
+      let imageType2 =
+        typeCouleur[donnees_localstorage[pokemon_id - 1].type[1]]?.image;
+      pokemonType.value = imageType1 || imageType2;
+
+      let ColorType1 =
+        typeCouleur[donnees_localstorage[pokemon_id - 1].type[0]]?.couleur;
+      let ColorType2 =
+        typeCouleur[donnees_localstorage[pokemon_id - 1].type[1]]?.couleur;
+      couleurBackground = ColorType1 || ColorType2;
+      console.log(couleurBackground);
     } catch (err) {
       error.value = err;
     } finally {
@@ -93,7 +187,11 @@ async function fetchPokemonList() {
 
 onMounted(() => {
   fetchPokemonList();
+
+  console.log(pokemonType.value);
 });
+
+// ../../public/image/types/plante.png
 </script>
 
 <template>
@@ -103,7 +201,8 @@ onMounted(() => {
     <div v-else-if="error">Une erreur est survenue : {{ error.message }}</div>
     <ul v-else>
       <article
-        class="m-2 border-8 border-lime-500 w-52 flex flex-col items-center rounded-lg"
+        :class="`${couleurBackground}`"
+        class="m-2 border-8 border-black w-56 flex flex-col items-center rounded-lg"
       >
         <section class="flex space-x-1 items-center">
           <p>
@@ -117,17 +216,20 @@ onMounted(() => {
                 : pokemonList.id
             }}
           </p>
-          <p class="font-bold">{{ pokemonList.name }}</p>
+          <p class="font-bold p-0.5">{{ pokemonList.name }}</p>
+
           <figure class="">
             <img
-              class="w-6"
-              src="../../public/image/types/plante.png"
-              :alt="pokemonList.name"
+              class="w-7 bg-black rounded-full p-0.5"
+              :src="`${pokemonType}`"
+              alt=""
             />
           </figure>
           <!-- <TypePokemon></TypePokemon> -->
         </section>
-        <figure class="border-2">
+        <!-- <figure class="border-2 border-black"> -->
+
+        <figure class="">
           <img
             class="w-36"
             :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList.id}.png`"
