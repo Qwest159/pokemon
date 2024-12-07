e250e1a1c11c0337c39c10131d2a5cf03f0b833a
Qwest159 <duchesne-raphael@hotmail.com>
2024-12-07 13:54:40 +0100

    nav

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,18 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="wrapper">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/pokedex">Pokedex</RouterLink>
+  <div class="wrapper bg-orange-300 p-3 mb-5">
+    <nav class="flex items-center">
+      <img
+        class="w-20 mr-3"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/83.png"
+        alt="Canarticho"
+      />
+      <p>
+        <RouterLink to="/">Home</RouterLink>
+      </p>
+      <p><RouterLink to="/pokedex">Pokedex</RouterLink></p>
+
       <!-- <RouterLink to="/show/2">Pokedex</RouterLink> -->
     </nav>
   </div>
@@ -14,8 +22,9 @@ import { RouterLink, RouterView } from "vue-router";
   <RouterView />
 </template>
 
-<style>
+<style scoped>
 p {
   @apply text-lg;
+  @apply text-slate-950;
 }
 </style>
